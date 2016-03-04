caced660d321d7c7fb55baba33092c2934ffa31f
volanStudio <lucas@volanstudio.com>
2016-03-04 13:26:24 -0800

    initial commit of sass setup

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+
+    // Sass to CSS
+    sass: {
+      app: {
+        files: [{
+          expand: true,
+          cwd: 'scss',
+          src: ['*.scss'],
+          dest: 'css',
+          ext: '.css'
+        }]
+      },
+      options: {
+        sourceMap: false,
+        outputStyle: 'compressed',
+        imagePath: "../",
+        includePaths: [
+          require('path').dirname(require.resolve('normalize.css'))
+        ]
+      }
+    },
+
+    watch: {
+      sass: {
+        files: ['scss/{,*/}*.{scss,sass}'],
+        tasks: ['sass']
+      },
+      options: {
+        livereload: true,
+        spawn: false
+      }
+    },
+  });
+
+  // Loads Grunt Tasks
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  grunt.registerTask('default', ['sass', 'watch']);
+};
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # boilerplate-int
 An early boilerplate for sass projects, with some customizations
+
+
+---
+Don't have livereload or a server in here yet, but you can compile and watch with:
+
+```
+grunt
+```
